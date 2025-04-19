93cd593a0656c0a19607c61b8e305f7da7cfff5f
hgene2452 <hgene0929@gmail.com>
2025-04-20 02:41:14 +0900

    init: initial commit

---
+++ src/main/kotlin/com/openapi/paypal_krpost/PaypalKrpostApplication.kt
@@ -0,0 +1,11 @@
+package com.openapi.paypal_krpost
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaypalKrpostApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaypalKrpostApplication>(*args)
+}
---
+++ src/test/kotlin/com/openapi/paypal_krpost/PaypalKrpostApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.openapi.paypal_krpost
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaypalKrpostApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
